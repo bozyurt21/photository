f886297e661a056c1d24142c6fcd478d60f4d719
bozyurt21 <bozyurt21@ku.edu.tr>
2025-09-16 20:14:43 +0300

    Changed PhotoView.swift file

--- PhotoScan/View/PhotoView.swift
+++ PhotoScan/View/PhotoView.swift
@@ -20,7 +20,7 @@ struct PhotoView: View {
                         .scaledToFill()
                         .clipped()
                 } else {
-                        Color.gray.opacity(0.3) // placeholder
+                        Color.gray.opacity(0.3) // if photo does not exist then we are going to show an gray color.
                 }
         }
         .frame(width: 100, height: 100)
